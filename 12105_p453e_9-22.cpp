9a96f5a32616313d42b2c5ea3e506da11abc78c9
ZhengChengkai826 <zchengkai826@gmail.com>
2019-11-27 17:44:11 +0800

    add

--- 12105_p453e_9-22.cpp
+++ 12105_p453e_9-22.cpp
@@ -10,8 +10,9 @@ using namespace std;
 const int maxn = 100 + 5;
 const int maxm = 3000 + 5;
 
-// dp[i][j] is the maximal length of the integer whose remainder is j (with at most i matches)
-// p[i][j] is the maximal digit for state (i,j)
+// dp[i][j=0] is the maximal length of the integer which is the multiple of m (with at most i matches)
+// p[i][j] is the maximal most-significant digit for state (i,j)
+// dp[i][j] can be regarded as the maximal length of the subpart of some dp[i][j=0]'s integer. the complementary subpart of that integer's remainder is j. (with at most i matches)
 int n, m, dp[maxn][maxm], p[maxn][maxm];
 
 int needs[] = { 6, 2, 5, 5, 4, 5, 6, 3, 7, 6 };
@@ -27,11 +28,18 @@ int main(){
         int& ans = dp[i][j];
         ans = p[i][j] = -1;
         if (j == 0) ans = 0;
+				/* 
+				 * initialize: p[i][j] always -1, dp[i][j] = 0 if j == 0 else -1 
+				 *             specifically, dp[0][0] = 0 and dp[0][>0] = -1
+				 */
+				 /* note: d is added on the left */
         for(int d = 9; d >= 0; d--)
           if (i >= needs[d]){
             int t = dp[i - needs[d]][(j * 10 + d) % m];
             if (t >= 0 && t + 1 > ans){
-              ans = t + 1;
+              //printf("\n**(i=%d, j=%d, d=%d, t=%d)**\n", i, j, d, t);
+							ans = t + 1;
+							//printf("\n**(dp[%d][%d]=dp[%d][%d]+1=%d+1=%d, p[%d][%d]=%d)", i, j, i-needs[d], (j*10+d)%m, t, ans, i, j, d);
               p[i][j] = d;
             }
           }
@@ -41,6 +49,7 @@ int main(){
     else {
       int i = n, j = 0;
       for(int d = p[i][j]; d >= 0; d = p[i][j]){
+      	//printf("\n**(dp[%d][%d]=%d)**\n", i, j, dp[i][j]);
         printf("%d", d);
         i -= needs[d];
         j = (j * 10 + d) % m;
